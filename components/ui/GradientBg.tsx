a433558cd4743259a6bd03ade8d1bc333c912027
Lakshit Rattan <lakshitrrattan@gmail.com>
2024-10-08 17:10:29 +0530

    ADD: Hero section complete

--- components/ui/GradientBg.tsx
+++ components/ui/GradientBg.tsx
@@ -6,7 +6,7 @@ export const BackgroundGradientAnimation = ({
   gradientBackgroundStart = "rgb(108, 0, 162)",
   gradientBackgroundEnd = "rgb(0, 17, 82)",
   firstColor = "18, 113, 255",
-  secondColor = "221, 74, 255",
+  secondColor = "221, 74, 255" ,
   thirdColor = "100, 220, 255",
   fourthColor = "200, 50, 50",
   fifthColor = "180, 180, 50",
@@ -89,7 +89,7 @@ export const BackgroundGradientAnimation = ({
   return (
     <div
       className={cn(
-        "h-screen w-screen relative overflow-hidden top-0 left-0 bg-[linear-gradient(40deg,var(--gradient-background-start),var(--gradient-background-end))]",
+        "h-full w-full absolute overflow-hidden top-0 left-0 bg-[linear-gradient(40deg,var(--gradient-background-start),var(--gradient-background-end))]",
         containerClassName
       )}
     >
