b55a28b9edfcc0f02206f903be68b0a7c085443a
Lakshit Rattan <lakshitrrattan@gmail.com>
2024-10-13 18:03:41 +0530

    ADD: Sentry intgration for crash reports and error handling, Client testimonials section

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image";
 import Hero from "@/components/Hero";
-import {Spotlight} from "@/components/ui/Spotlight";
 import { FloatingNav } from "@/components/ui/FloatingNav";
-import { FaHome } from "react-icons/fa";
 import Grid from "@/components/Grid";
+import RecentProjects from "@/components/RecentProjects";
+import { navItems } from "@/components/data";
+import Clients from "@/components/Clients";
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]} />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
+        <RecentProjects />
+        <Clients />
       </div>
     </main>
   );
