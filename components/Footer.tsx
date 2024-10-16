95580ff52a8e404ff7a4f4d49aa9afa55bb7e8a1
Lakshit Rattan <lakshitrrattan@gmail.com>
2024-10-17 03:03:37 +0530

    FIX: image alt tag typeError fixed in footere

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -42,10 +42,10 @@ const Footer = () => {
               key={profile.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop:blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <Link target='_blank' href={profile.link}>
+              <Link target="_blank" href={profile.link}>
                 <img
                   src={profile.img}
-                  alt={profile.id}
+                  alt={`${profile.id}`}
                   width={20}
                   height={20}
                 />
